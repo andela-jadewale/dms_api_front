b17806df42ccc1f3bf6228f74ceafaa4ab5788be
Jolaade Adewale <jolaade.adewale@andela.com>
2016-07-18 15:27:39 +0100

    [126487589] set up concatenation, minification of js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+var path = require('path');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var paths = {
+    public: 'public/**',
+    jade: ['!app/shared/**', 'app/**/*.jade'],
+    scripts: 'app/**/*.js',
+    images: 'app/images/**/*',
+    staticFiles: [
+      '!app/**/*.+(less|css|js|jade)',
+      '!app/images/**/*',
+      'app/**/*.*'
+    ],
+    unitTests: [],
+    serverTests: ['./tests/server/**/*.spec.js'],
+    libTests: ['lib/tests/**/*.js'],
+    styles: 'app/styles/*.+(less|css)'
+  };
+
+// minimise app.js
+gulp.task('min', function() {
+    return gulp.src('./.tmp/app.js')
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest('./.tmp'));
+});
 
 //restart server when we change our server.js file
 gulp.task('live-server', function () {
@@ -8,7 +39,22 @@ gulp.task('live-server', function () {
   server.start();
 })
 
-gulp.task('serve', ['live-server'], function () {
+gulp.task('copy', function () {
+  gulp.src(['app/styles/*.css']).pipe(gulp.dest('./.tmp'))
+})
+
+gulp.task('bundle',['copy'], function () {
+  return browserify({
+    entries: 'app/main.jsx',
+    'debug': true,
+  })
+  .transform(reactify)
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./.tmp'))
+})
+
+gulp.task('serve', ['bundle','min','live-server'], function () {
   browserSync.init(null, {
     proxy: 'http://localhost:8084',
     port: 9001
